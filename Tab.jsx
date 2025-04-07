bb61b56e551a26704890531ad250128712cd5e4e
Chatresh Konchada <hemishkonchada@gmail.com>
2025-04-07 23:37:10 +0530

    Create Tab.jsx
    React Tab Components

---
+++ Tab.jsx
@@ -0,0 +1,31 @@
+import React, { useState } from 'react';
+
+const TabComponent = () => {
+  const [activeTab, setActiveTab] = useState('Tab1');
+
+  return (
+    <div>
+      {/* Tabs */}
+      <div>
+        <button onClick={() => setActiveTab('Tab1')}>
+          Tab 1
+        </button>
+        <button onClick={() => setActiveTab('Tab2')}>
+          Tab 2
+        </button>
+        <button onClick={() => setActiveTab('Tab3')}>
+          Tab 3
+        </button>
+      </div>
+
+      {/* Content */}
+      <div>
+        {activeTab === 'Tab1' && <p>This is content for Tab 1</p>}
+        {activeTab === 'Tab2' && <p>This is content for Tab 2</p>}
+        {activeTab === 'Tab3' && <p>This is content for Tab 3</p>}
+      </div>
+    </div>
+  );
+};
+
+export default TabComponent;
